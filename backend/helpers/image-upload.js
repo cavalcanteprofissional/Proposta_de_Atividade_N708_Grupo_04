c34d640b45116ac262d1738d03992b80400d523b
Lucas Cavalcante dos Santos <cavalcanteprofissional@outlook.com>
2025-06-03 08:19:09 -0300

    correções

--- backend/helpers/image-upload.js
+++ backend/helpers/image-upload.js
@@ -1,37 +1,34 @@
-const multer = require('multer')
-const path = require('path')
+const multer = require("multer");
+const path = require("path");
 
-//Destination to store the imagens
-
+// Destination to store image
 const imageStorage = multer.diskStorage({
-    destination: function (req, file, cb) {
+  destination: function (req, file, cb) {
+    let folder = "";
 
-        let folder = ""
+    console.log(req)
 
-        console.log(req)
+    if (req.baseUrl.includes('users')) {
+      folder = "users";
+    } else if (req.baseUrl.includes('pets')) {
+      folder = "pets";
+    }
+    cb(null, `public/images/${folder}/`);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
 
-        if (req.baseUrl.includes('users')) {
-            folder = 'users'
-        } else if (req.baseUrl.includes('pets')) {
-            folder = 'pets'
-        }
-        cb(null, `public/images/${folder}/`)
-    },
-    filename: function(req, file, cb) {
-        cb(null, Date.now() + 
-        String(Math.floor(Math.random() * 1000)) +
-        path.extname(file.originalname))
-    },
+const imageUpload = multer({
+  storage: imageStorage,
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg)$/)) {
+      // upload only png and jpg format
+      return cb(new Error("Por favor, envie apenas png ou jpg!"));
+    }
+    cb(undefined, true);
+  },
+});
 
-})
-const imageUpload = multer ({
-    storage : imageStorage,
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(png|jpg)$/)) {
-            return cb(new Error('só aceitamos imagens com extensões .jpg e .png'))
-            
-        }
-        cb(undefined, true)
-    },
-})
-module.exports = { imageUpload }
+module.exports = { imageUpload };
